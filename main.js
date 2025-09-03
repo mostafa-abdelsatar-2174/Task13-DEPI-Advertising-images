9825002a0b1d161cac2a5ea734e1044270d26720
Mostafa Mohammed Abdelsatar <mosasa1112@gmail.com>
2025-09-03 16:02:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var ImgsContainer = document.querySelector("#imgs-container");
 var imgtag = document.querySelector('img');
+var thebutton = document.querySelector("button");
 
 console.log(imgtag);
 console.log(imgtag.src);
 console.log(ImgsContainer);
-// console.log(imgtag.currentSrc);
+console.log(thebutton);
+console.log(thebutton.innerHTML);
 
+// console.log(imgtag.currentSrc);
 var ImgsUrl =[
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf3fTonNAxovtprsexDHEdS2-znhWaewwm6w&s",
     "https://english.news.cn/20250408/99961d4c7fdf4edebb0c357196ced5ad/uPQOUm67nWPUS1UP.jpg",
@@ -15,13 +18,35 @@ var ImgsUrl =[
 ];
 
 var count=0;
-var imgsinterval = setInterval(function(){
-    imgtag.src=ImgsUrl[count];
-    count++;
-    if(count==5){
-        count=0;    
+var intervaltest=true;
+var imgsinterval;
+
+function TestTheInterval(){
+    // console.log("sasa");
+    if(intervaltest){
+        thebutton.innerHTML=`End the images interval`
+        thebutton.style.backgroundColor="red";
+        imgsinterval=setInterval(function(){
+            imgtag.src=ImgsUrl[count];
+            count++;
+            if(count==5){
+                count=0;    
+            }
+            
+            
+        },2000);
+        intervaltest=false;
+        console.log(intervaltest);
     }
-},2000);
+    else{
+        thebutton.innerHTML=`Start The images interval`
+        thebutton.style.backgroundColor="chartreuse";
+        clearimgsinterval();
+        intervaltest=true;
+        console.log(intervaltest);
+        
+    }
+}
 
 function clearimgsinterval(){
     clearInterval(imgsinterval);
